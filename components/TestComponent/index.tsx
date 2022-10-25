9814a090f5b43a5001b47d9208a44d26314bf8fa
devUserContact <developer.user.contact@protonmail.com>
2022-10-25 07:36:01 -0700

    initial commit

---
+++ components/TestComponent/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import emoji from "../../assets/emoji.json";
+import styles from "../../styles/Main.module.scss";
+
+const TestComponent = () => {
+	console.table(emoji);
+	return (
+		<div className={styles.textContainer}>
+			<p className={styles.paragraphText}>
+				Sphinx of black quartz, judge my vow.
+			</p>
+		</div>
+	);
+};
+
+export default TestComponent;
