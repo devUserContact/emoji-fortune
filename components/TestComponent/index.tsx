02cb3ec42daafd041a7ef74ab3d29498bbc50bdb
devUserContact <developer.user.contact@protonmail.com>
2022-10-25 10:23:58 -0700

    implemented emoji.json in component

--- components/TestComponent/index.tsx
+++ components/TestComponent/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import emoji from "../../assets/emoji.json";
+import { emoji } from "../../assets/emoji.json";
 import styles from "../../styles/Main.module.scss";
 
 const TestComponent = () => {
-	console.table(emoji);
+	console.table(emoji.objects[0]);
 	return (
 		<div className={styles.textContainer}>
 			<p className={styles.paragraphText}>
