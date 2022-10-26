595a29ef068601a81faed1970d689dc0d530321d
devUserContact <developer.user.contact@protonmail.com>
2022-10-26 07:37:21 -0700

    implemented useEffect

--- components/EmojiFortune/index.tsx
+++ components/EmojiFortune/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useCallback } from "react";
+import { useEffect } from "react";
 import { emoji } from "../../assets/emoji.json";
 import styles from "../../styles/Main.module.scss";
 
@@ -9,19 +9,23 @@ const EmojiFortune = () => {
 	let fortuneDraw = [];
 	let fortuneArray = [];
 	const randSet = new Set();
+	useEffect(() => {
+	}, [])
 
 	emoji.map((emoji) => {
 		if (emoji.checked === true) {
 			emojiArray.push(...emoji.catalog);
 		}
 	});
+	
+	randomGen(3, emojiArray.length);
 	function randomGen(quantity, max) {
 		while (randSet.size < quantity) {
 			randSet.add(Math.floor(Math.random() * max) + 1);
 		}
 		return randSet;
 	}
-	randomGen(3, emojiArray.length);
+
 	randSet.forEach((key) => {
 		let emojiObject = {
 			pictogram: emojiArray[key].pictogram,
@@ -33,7 +37,6 @@ const EmojiFortune = () => {
 	return (
 		<div className={styles.textContainer}>
 			<p className={styles.paragraphText}>{
-			fortuneArray[1].pictogram
 			}</p>
 		</div>
 	);
