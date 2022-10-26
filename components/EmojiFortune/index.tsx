d15679c753f01bde2ce755b440d070d9442bae89
devUserContact <developer.user.contact@protonmail.com>
2022-10-25 19:59:17 -0700

    prototype random emoji function

--- components/EmojiFortune/index.tsx
+++ components/EmojiFortune/index.tsx
@@ -1,30 +1,40 @@
 import React from "react";
+import { useCallback } from "react";
 import { emoji } from "../../assets/emoji.json";
 import styles from "../../styles/Main.module.scss";
 
 const EmojiFortune = () => {
-//  const [smileysAndPeople, setSmileysAndPeople] = useState(true);
-//  const [, set] = useState(true);
-//  const [, set] = useState(true);
-//  const [, set] = useState(true);
-//  const [, set] = useState(true);
-//  const [, set] = useState(true);
-//  const [, set] = useState(true);
-//  const [, set] = useState(true);
-//	emojiCategories =	Object.keys(emoji)
+
+	let emojiArray = [];
+	let fortuneDraw = [];
 	let fortuneArray = [];
+	const randSet = new Set();
+
 	emoji.map((emoji) => {
-		console.table(emoji.category)
-		if (emoji.checked === true ) {
-			fortuneArray.push(...emoji.catalog)
+		if (emoji.checked === true) {
+			emojiArray.push(...emoji.catalog);
 		}
-	})
-	console.log(fortuneArray)
+	});
+	function randomGen(quantity, max) {
+		while (randSet.size < quantity) {
+			randSet.add(Math.floor(Math.random() * max) + 1);
+		}
+		return randSet;
+	}
+	randomGen(3, emojiArray.length);
+	randSet.forEach((key) => {
+		let emojiObject = {
+			pictogram: emojiArray[key].pictogram,
+			unicode: emojiArray[key].unicode,
+		}
+		fortuneArray.push(emojiObject);
+	});
+
 	return (
 		<div className={styles.textContainer}>
-			<p className={styles.paragraphText}>
-				{fortuneArray[308].pictogram}
-			</p>
+			<p className={styles.paragraphText}>{
+			fortuneArray[1].pictogram
+			}</p>
 		</div>
 	);
 };
