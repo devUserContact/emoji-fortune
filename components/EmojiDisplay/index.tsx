c3156fc72c1944cca25f50675015361a14c18c5b
devUserContact <developer.user.contact@protonmail.com>
2022-10-28 13:53:45 -0700

    implemented hide and show emojiDescription

--- components/EmojiDisplay/index.tsx
+++ components/EmojiDisplay/index.tsx
@@ -4,8 +4,24 @@ import styles from "../../styles/Main.module.scss";
 
 const EmojiDisplay = ({ emojiItem }: any) => {
 	const [isShown, setIsShown] = useState(false);
+
+	const showDescription = useCallback(() => {
+		setIsShown((prevValue) => !prevValue);
+	}, []);
+
 	return (
-			<p className={styles.paragraphText}>{emojiItem.pictogram}</p>
+		<div
+			className={styles.emojiContainer}
+			onMouseEnter={showDescription}
+			onMouseLeave={showDescription}
+		>
+			<p className={styles.pictogramText}>{emojiItem.pictogram}</p>
+			{isShown && (
+				<div className={styles.descriptionContainer}>
+					<p className={styles.descriptionText}>{emojiItem.description}</p>
+				</div>
+			)}
+		</div>
 	);
 };
 
