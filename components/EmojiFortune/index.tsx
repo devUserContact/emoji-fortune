ee5cbfc8ff25430cef36dab283e0c56ae16875f7
devUserContact <developer.user.contact@protonmail.com>
2022-10-29 12:56:55 -0700

    refined revealFortune function

--- components/EmojiFortune/index.tsx
+++ components/EmojiFortune/index.tsx
@@ -1,41 +1,31 @@
 import React from "react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import EmojiDisplay from "../EmojiDisplay";
 import { emoji } from "../../assets/emoji.json";
 import styles from "../../styles/Main.module.scss";
 
 const EmojiFortune = () => {
 	const [fortune, setFortune] = useState([{}]);
-	const [isShown, setIsShown] = useState(false);
-	const generateFortune = () => {
-		let emojiArray: {
-			pictogram: string;
-			CLDR: string;
-			unicode: string;
-			"UTF-8": string;
-		}[] = [];
-		let fortuneArray: {
-			pictogram: any;
-			description: any;
-			unicode: any;
-			"UTF-8": any;
-		}[] = [];
-		const randSet = new Set();
-		emoji.map((emoji) => {
-			if (emoji.checked === true) {
-				emojiArray.push(...emoji.catalog);
-			}
-		});
-
-		randomGen(5, emojiArray.length);
-
-		function randomGen(quantity: number, max: number) {
-			while (randSet.size < quantity) {
-				randSet.add(Math.floor(Math.random() * max) + 1);
-			}
-			return randSet;
+	let emojiArray: {
+		pictogram: string;
+		CLDR: string;
+		unicode: string;
+		"UTF-8": string;
+	}[] = [];
+	let fortuneArray: {
+		pictogram: any;
+		description: any;
+		unicode: any;
+		"UTF-8": any;
+	}[] = [];
+	let randSet = new Set();
+	emoji.map((emoji) => {
+		if (emoji.checked === true) {
+			emojiArray.push(...emoji.catalog);
 		}
-
+	});
+	const generateFortune = () => {
+		randomGen(5, emojiArray.length);
 		randSet.forEach((randNum: any) => {
 			let emojiObject = {
 				pictogram: emojiArray[randNum].pictogram,
@@ -46,17 +36,21 @@ const EmojiFortune = () => {
 			fortuneArray.push(emojiObject);
 		});
 		setFortune(fortuneArray);
+		randSet.clear();
+		fortuneArray = [];
 	};
-	useEffect(() => {
-		generateFortune();
-	}, []);
-	let refresh = () => {
-		setFortune([]);
+	const revealFortune = () => {
 		generateFortune();
 	};
+	function randomGen(quantity: number, max: number) {
+		while (randSet.size < quantity) {
+			randSet.add(Math.floor(Math.random() * max) + 1);
+		}
+		return randSet;
+	}
 	return (
 		<>
-			<button className={styles.bttn} type="button" onClick={refresh}>
+			<button className={styles.bttn} type="button" onClick={revealFortune}>
 				reveal fortune
 			</button>
 			<div className={styles.textContainer}>
